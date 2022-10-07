1d9c11485233dfdcf4078730822df541ba7fe1aa
Maycon <73504705+dualsgo@users.noreply.github.com>
2022-10-06 23:24:53 -0300

    null

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-function funcao1() {
-    alert("Obrigado pelo contato! Redirecionando para a página principal");
-}
+
 
 function mensagemCompra() {
     confirm("Seu produto foi adicionado ao carrinho!")
